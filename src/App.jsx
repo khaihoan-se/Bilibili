88064f21a901e481a5513c7beb0476a4abf0c084
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-03-24 20:07:15 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Narbar from './components/Narbar'
+
+
+const App = () => {
+   return (
+      <div className="app">
+         <Narbar />
+      </div>
+   )
+}
+
+export default App
---
+++ src/components/Narbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LangIcon from '../asset/icons/LangIcon';
+import SearchIcon from '../asset/icons/SearchIcon';
+
+const Narbar = () => {
+  return (
+    <div>
+      <SearchIcon className="icon__header" />
+      <LangIcon className="icon__header" />
+    </div>
+  );
+}
+
+export default Narbar;
